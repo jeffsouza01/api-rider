66989a9d007098d542f7e7fda48e20a3f1bff3a3
Jefferson da Silva <je_jss@hotmail.com>
2021-08-05 21:11:35 -0300

    update schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,11 +1,12 @@
 import { buildSchemaSync } from "type-graphql";
 
 import { AuthController } from "../controllers/AuthController";
+import { PedalController } from "../controllers/PedalController";
 import { UsersController } from "../controllers/UsersController";
 import ensureAuthenticated from "../middleware/ensureAuthenticated";
 
 const schema = buildSchemaSync({
-  resolvers: [UsersController, AuthController],
+  resolvers: [UsersController, AuthController, PedalController],
   authChecker: ensureAuthenticated,
 });
 
