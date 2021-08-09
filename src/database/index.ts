f5ccd379b9fad634351e25a1fdf2c54c016dd12c
Jefferson da Silva <je_jss@hotmail.com>
2021-08-09 18:57:38 -0300

    Update Config Database

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,8 +4,7 @@ export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   const connection = Object.assign(defaultOptions, {
-    database:
-      process.env.NODE_ENV === "test" ? "rider_tests" : defaultOptions.database,
+    database: process.env.NODE_ENV,
   });
 
   return createConnection(connection);
